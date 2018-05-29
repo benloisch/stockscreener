ea006f6b5c0d0b9ae431ecf547868d5fe7c9ff74
Benjamin Loisch <ben.loisch@gmail.com>
2018-05-28 19:47:35 -0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <io.h>
+#include <fstream>
 using namespace std;
 
 #include "csv.h"
 
 class Equity {
 public:
+	string industry;
+	unsigned long long marketCap;
+	unsigned int lengthOfDays;
 	string ticker;
 	vector<string> date;
 	vector<float> open;
@@ -17,7 +21,16 @@ public:
 	vector<float> low;
 	vector<float> close;
 	vector<unsigned int> volume;
+
+	Equity() {
+		industry = "NA";
+		marketCap = 0;
+	}
 };
+
+vector<string> preTicker;
+vector<unsigned long long> currentMarketCap;
+vector<string> tickerIndustry;
 
 vector<Equity> stocks;
 vector<Equity> etfs;
@@ -26,8 +39,31 @@ unsigned int etfLoadIndex = 0;
 
 unsigned int loadXEquities = 0;
 
-void loadEquityFolder(string folderPath, string equityType) {
+void loadEquityFolder(string folderPath, string equityType, string specificEquity) {
 	string directory = folderPath;
+
+	io::CSVReader<3> inCapAndIndustry("C:\\Users\\benlo\\Documents\\Stock Data\\data\\tickerData\\tickerCapIndustry.csv");
+	try {
+		inCapAndIndustry.read_header(io::ignore_extra_column, "Ticker", "Market Cap", "Industry");
+	}
+	catch (...) {
+		cout << "Error reading C:\\Users\\benlo\\Documents\\Stock Data\\data\\tickerData\\tickerCapIndustry.csv" << endl;
+	}
+
+	std::string tickerName;
+	std:string i_marketCap;
+	std::string industryString;
+	while (inCapAndIndustry.read_row(tickerName, i_marketCap, industryString)) {
+		preTicker.push_back(tickerName);
+		if (i_marketCap != "") {
+			currentMarketCap.push_back(stoull(i_marketCap));
+		}
+		else {
+			currentMarketCap.push_back(0);
+		}
+		
+		tickerIndustry.push_back(industryString);
+	}
 
 	_finddata_t info;
 	intptr_t hFile;
@@ -40,6 +76,12 @@ void loadEquityFolder(string folderPath, string equityType) {
 		_findnext(hFile, &info); //read "." hidden directory file
 
 		while (_findnext(hFile, &info) == 0) {
+
+			if (specificEquity != "") {
+				if (info.name != specificEquity) {
+					continue;
+				}
+			}
 
 			//loadXEquities++;
 			//if (loadXEquities > 500) {
@@ -65,6 +107,29 @@ void loadEquityFolder(string folderPath, string equityType) {
 				stocks[stockLoadIndex].ticker = tickerUpper;
 			}
 
+			ifstream lines;
+			string emptyString;
+			unsigned int lineCount = 0;
+			lines.open(directory + tickerLower + ".us.txt");
+
+			while (!lines.eof()) {
+				getline(lines, emptyString);
+				lineCount++;
+			}
+
+			if (equityType == "etf") {
+				etfs[etfs.size() - 1].lengthOfDays = lineCount;
+			}
+			else {
+				stocks[stocks.size() - 1].lengthOfDays = lineCount;
+				stocks[stockLoadIndex].close.reserve(lineCount);
+				stocks[stockLoadIndex].open.reserve(lineCount);
+				stocks[stockLoadIndex].high.reserve(lineCount);
+				stocks[stockLoadIndex].low.reserve(lineCount);
+				stocks[stockLoadIndex].date.reserve(lineCount);
+				stocks[stockLoadIndex].volume.reserve(lineCount);
+			}
+
 			io::CSVReader<7> in(directory + tickerLower + ".us.txt");
 			try {
 				in.read_header(io::ignore_extra_column, "Date", "Open", "High", "Low", "Close", "Volume", "OpenInt");
@@ -72,6 +137,25 @@ void loadEquityFolder(string folderPath, string equityType) {
 			catch (...) {
 				cout << "Error reading " + directory + tickerLower + ".us.txt" << endl;
 			}
+
+			int i = 0;
+			for (; i < preTicker.size(); i++) {
+				if (preTicker[i] == tickerUpper) {
+					break;
+				}
+			}
+
+			if (i < preTicker.size()) {
+				if (equityType == "etf") {
+					etfs[etfLoadIndex].marketCap = currentMarketCap[i];
+					etfs[etfLoadIndex].industry = tickerIndustry[i];
+				}
+				else {
+					stocks[stockLoadIndex].marketCap = currentMarketCap[i];
+					stocks[stockLoadIndex].industry = tickerIndustry[i];
+				}
+			}
+
 			std::string date;
 			float open, high, low, close, openint;
 			unsigned int volume;
@@ -111,18 +195,39 @@ int main() {
 
 	clock_t begin = clock();
 
-	loadEquityFolder("C:\\Users\\benlo\\Documents\\Stock Data\\data\\daily\\us\\nasdaq stocks\\2\\", "stock");
+	string specificEquity = "tde.us.txt";
+	specificEquity = "";
 
-
+	//stock
+	//loadEquityFolder("C:\\Users\\benlo\\Documents\\Stock Data\\data\\daily\\us\\nasdaq stocks\\1\\", "stock", specificEquity);
+	loadEquityFolder("C:\\Users\\benlo\\Documents\\Stock Data\\data\\daily\\us\\nasdaq stocks\\2\\", "stock", specificEquity);
+	//loadEquityFolder("C:\\Users\\benlo\\Documents\\Stock Data\\data\\daily\\us\\nyse stocks\\1\\", "stock", specificEquity);
+	//loadEquityFolder("C:\\Users\\benlo\\Documents\\Stock Data\\data\\daily\\us\\nyse stocks\\2\\", "stock", specificEquity);
+	
+	//index
+	//loadEquityFolder("C:\\Users\\benlo\\Documents\\Stock Data\\data\\daily\\us\\nasdaq etfs\\", "etf", specificEquity);
+	//loadEquityFolder("C:\\Users\\benlo\\Documents\\Stock Data\\data\\daily\\us\\nyse etfs\\", "etf", specificEquity);
+	
 	clock_t end = clock();
 	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
 	cout << elapsed_secs << endl;
-
+	
 	int greenRedCount = 0;
 	float greenRedTotal = 0;
 
+	string tickerHighest = "";
+	float greenOverRed = 0.0;
+
 	for (int i = 0; i < stocks.size(); i++) {
+
+		if (stocks[i].volume.size() < 1825 || stocks[i].volume.size() > 2190) {
+			continue;
+		}
+
+		//if (stocks[i].ticker != "LPLA") {
+		//	continue;
+		//}
 
 		if (stocks[i].close.size() == 0) {
 			continue;
@@ -131,16 +236,17 @@ int main() {
 		if (stocks[i].close[stocks[i].close.size() - 1] < 5)
 			continue;
 
-		cout << "Analyzing Stock: " + stocks[i].ticker << endl;
-
 		float green = 0;
 		float red = 0;
 
 		for (int d = 1; d < stocks[i].volume.size() - 1; d++) {
 			if (stocks[i].close[d - 1] < stocks[i].open[d - 1]) {
 				if (stocks[i].close[d] > stocks[i].open[d]) {
+					//if (stocks[i].open[d] < stocks[i].close[d - 1]) {
+						//if (stocks[i].close[d] > stocks[i].open[d - 1]) {
 					if (stocks[i].low[d] < stocks[i].low[d - 1] && stocks[i].open[d] < stocks[i].close[d - 1]) {
 						if (stocks[i].high[d] > stocks[i].high[d - 1] && stocks[i].close[d] > stocks[i].open[d - 1]) {
+							//cout << stocks[i].date[d] << endl;
 							if (stocks[i].close[d + 1] > stocks[i].open[d + 1]) {
 								green++;
 							}
@@ -155,12 +261,19 @@ int main() {
 
 		if (green / red >= 0.0001 && green / red < 100) {
 			//cout << "G/R After Bullish Engulfing is: " + to_string(green / red) << endl;
+			cout << "Analyzing Stock: " + stocks[i].ticker + " G/R: " + to_string(green / red) << endl;
 			greenRedCount++;
 			greenRedTotal += (green / red);
+
+			if (green / red > greenOverRed) {
+				tickerHighest = stocks[i].ticker;
+				greenOverRed = green / red;
+			}
 		}
 	}
 
 	cout << "Results: " + to_string(greenRedTotal / greenRedCount) << endl;
+	cout << "Highest: " + tickerHighest + " with ratio: " + to_string(greenOverRed) << endl;
 
 	//unsigned int totalDays = 0;
 	//for (int i = 0; i < stocks.size(); i++) {
@@ -168,7 +281,7 @@ int main() {
 		//cout << stocks[i].ticker + ", days: " + to_string(stocks[i].volume.size()) << endl;
 	//}
 	
-
+	
 	system("PAUSE");
 	return 0;
 }
